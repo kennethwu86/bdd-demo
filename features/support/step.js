4b32cce3507454ebb83c2643aaba0bfe565489bb
Kenneth Wu <kenneth.wu@ichef.com.tw>
2020-02-18 14:40:12 +0800

    Add git ignore

--- features/support/step.js
+++ features/support/step.js
@@ -1,12 +1,7 @@
 const { Given, When, Then } = require('cucumber')
 const expect = require('expect')
 
-var targetVal = null
-
 Given('Please print: {string}', function (printString) {
-    //this.printString = printString
-    //printString = printString
-
     this.targetVal = printString
 })
 
@@ -15,7 +10,6 @@ When('Print string', function () {
 })
 
 Then('Display string: {string}', function (expectedString) {
-    //expect(this.printString).toHaveLength(1);
     expect(this.targetVal).toEqual(expectedString)
 })
 
